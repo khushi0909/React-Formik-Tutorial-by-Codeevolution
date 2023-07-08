9b04ca03e833076743d789dfdbcf4267f9108d9f
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-07-08 18:02:34 +0200

    handleSubmit added

--- formik/Episode5/src/Components/YouTubeForm.js
+++ formik/Episode5/src/Components/YouTubeForm.js
@@ -8,19 +8,22 @@ function YouTubeForm() {
             name:'khushboo',
             email:'',
             channel:''
+        },
+        onSubmit: values=>{
+            console.log("Form Values",values)
         }
     });
-    console.log("values:", formik.values)
+    // console.log("values:", formik.values)
   return (
     <div>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <label htmlFor='name'>Name</label>
             <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name}/>
 
             <label htmlFor = 'email'>Email</label>
             <input type='email' name='email' id='email' onChange={formik.handleChange} value={formik.values.email}/>
 
-            <label htmlFor='channel'>Name</label>
+            <label htmlFor='channel'>Channel</label>
             <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel}/>
 
             <button>Submit</button>
