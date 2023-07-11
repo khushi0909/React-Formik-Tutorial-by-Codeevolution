ea4f9963efd06625e62014ef25e6be289ca5274b
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-07-11 22:33:14 +0200

    registration form

--- formik-controls-demo/Episode-39-44/src/App.js
+++ formik-controls-demo/Episode-39-44/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm/>
+      {/* <LoginForm/> */}
+      <RegistrationForm/>
     </div>
   );
 }
